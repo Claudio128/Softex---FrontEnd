072a28570ff3654a7406283f84cce799713f6f70
claudio128 <clladio128@gmail.com>
2022-08-02 23:34:21 -0300

    02/08/2022

---
+++ Atividade 4 - Lógica de Programação e Orientação a Objetos.py
@@ -0,0 +1,33 @@
+print("******************************");
+print("*      Cadastro Pessoal      *");
+print("******************************");
+
+nome = input("Digite seu nome: ");
+print(nome);
+
+endereco = input("Digite seu endereço: ");
+print(endereco);
+
+cidade = input("Digite sua cidade: ");
+print(cidade);
+
+cpf = input("Digite seu cpf: ");
+print(cpf);
+
+rg = input("Digite seu rg: ");
+print(rg);
+
+idade = input("Digite sua idade: ");
+print(idade);
+
+pai = input("Digite o nome do seu pai: ");
+print(pai);
+
+mae = input("Digite o nome da sua mãe: ");
+print(mae);
+
+peso = input("Digite seu peso: ");
+print(peso);
+
+renda = input("Digite sua renda: ");
+print(renda);
